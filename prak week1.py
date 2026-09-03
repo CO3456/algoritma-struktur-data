e4abf47dee642dcd727f562e196302ce79617766
CO3456 <francescoryan123@gmail.com>
2026-09-03 14:28:23 +0700

    Create prak week1.py

---
+++ prak week1.py
@@ -0,0 +1,15 @@
+def piramida_angka(n):
+
+    for i in range(1, n+1):
+        for j in range(n - i):
+            print(" ", end=" ")
+
+        for k in range(1, i + 1):
+            print(k, end=" ")
+
+        for l in range(i - 1, 0, -1):
+            print(l, end=" ")
+        print()
+    
+
+piramida_angka(9)
